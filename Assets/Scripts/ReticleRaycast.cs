56a42e3aab275c9cb11c30334cf23b6daf11cba5
Nstjacques <Nstjacques15bf@gmail.com>
2017-07-20 15:12:20 -0400

    New prefabs, raycasting enabled, destroying objects enabled

---
+++ Assets/Scripts/ReticleRaycast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Hi Nick! This is a script for always running a raycast in FixedUpdate that checks if an object has a tag -
+ if it does, then it returns a boolean to a GameManager script as True, so the player can click!"
+ Attach this to your main camera object. */
+
+public class ReticleRaycast : MonoBehaviour {
+
+public gameManager gameManager;
+public Image AspectRatio;
+private Ray theRay;
+
+void FixedUpdate() 
+{	
+	RaycastHit hit = new RaycastHit();
+
+	if (Physics.Raycast(transform.position, transform.forward, out hit)){
+		gameManager.hitObject = hit.collider.gameObject;
+		//print("There is something in front of the object!");
+		if (hit.collider.gameObject.tag == "inventoryItem"){
+			//print ("Targetable object found");
+			//AspectRatio.rectTransform.localScale = new Vector3(1,1,1);
+			gameManager.canClick = true;
+				Debug.Log ("Spooky");
+				gameManager.currentClick = hit.transform.gameObject;
+		}
+		else {
+			//AspectRatio.rectTransform.localScale = new Vector3(1,1.35f,1);
+			gameManager.canClick = false;
+		}
+	}
+}
+}
