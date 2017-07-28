a20af99690b766a319625bd655e135364a103397
Ethan Thibault <wickedlystudios@gmail.com>
2017-07-28 12:11:20 -0400

    MainMenu Created with Prefab
    - Created new scene for Main Menu
    - Created Main Menu prefab
    - Created folder for UI prefabs
    - Wrote UI_Functions
    - Added reticle to player prefab

---
+++ Assets/Scripts/UI_Functions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Functions : MonoBehaviour {
+	public GameObject ControlsPanelObj;
+
+	// TODO: Make update function that listens for "Cancel" button disables panels if they are active
+
+	/* Main Menu */
+	public void PlayGame(){
+		SceneManager.LoadScene(1);
+	}
+	public void ControlsPanel(){
+		//	TODO: Find the Controls Panel, set active
+		ControlsPanelObj.SetActive(true);
+		
+	}
+
+	/* Pause Menu */
+	public void Restart(){
+		// Reloads the current scene
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+	public void ExittoMain(){
+		// Loads Main Menu
+		SceneManager.LoadScene(0);
+	}
+
+	/* Both */
+	public void Quit(){
+		Application.Quit();
+	}
+}
