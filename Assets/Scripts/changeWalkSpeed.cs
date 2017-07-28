062a041029802da89176deb20115fa9ee848de00
Ethan Thibault <wickedlystudios@gmail.com>
2017-07-28 14:40:52 -0400

    New FPS Controller, Housekeeping
    - Imported Unity FPS Controller
    - Got a function to slow down working
    - Deleted First Person Drifter
    - Deleted SMSS
    - Fixed Prefab Names

---
+++ Assets/Scripts/changeWalkSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeWalkSpeed : MonoBehaviour {
+
+	public UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.T)){
+			changeSpeed(1f);
+		}	
+	}
+
+	private void changeSpeed(float newSpeed){
+		controller.m_WalkSpeed -= newSpeed;
+	}
+}
--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -7,24 +7,14 @@ public class gameManager : MonoBehaviour {
 	public bool canClick;
 	public bool hitObject;
 	public GameObject currentClick;
-	public GameObject SecondPersonDrifter;
-	private FirstPersonDrifter FirstPersonDrifter;
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 
 	void Awake() {
-		FirstPersonDrifter = SecondPersonDrifter.GetComponent<FirstPersonDrifter> ();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1") && canClick == true) {
-			Destroy (currentClick);
-			FirstPersonDrifter.speed -= 3;
-			Debug.Log ("Sloth");
-		}
 	}
 }
