fd0ec2d652bf629c4a865be7e18281230d4cbcec
Ethan Thibault <wickedlystudios@gmail.com>
2017-08-05 13:14:59 -0400

    UI Message Fades in!

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -5,22 +5,36 @@ using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour {
 
+	[Header("UI Fade Time")]
+	[SerializeField]
+	// I really don't get why this has to be so large, but now it looks okay!
+	private float ui_fade_timer = 50f;
+
+	[Header("UI Display Time")]
+	[SerializeField]
+	private float ui_display_time = 4f;
+
+	[Header("Message")]
 	public GameObject panel;
 	public Text text;
-	// Use this for initialization
+	
+	/* Private */
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
 
 	public IEnumerator Message (string message) {
-		panel.SetActive(true);
 		text.text = message;
-		yield return new WaitForSeconds (4);
+		Image img = panel.GetComponent<Image>();
+		panel.SetActive(true);
+		img.CrossFadeAlpha(255, ui_fade_timer, true);
+		yield return new WaitForSeconds (ui_display_time);
+		img.CrossFadeAlpha(0f, ui_fade_timer, true);
 		panel.SetActive (false);
 	}	
 }
