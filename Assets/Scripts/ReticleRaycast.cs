60f721c478ed1c63aa416bf2d60f54ace93818de
Nstjacques <Nstjacques15bf@gmail.com>
2017-08-10 12:41:46 -0400

    Quick Fixes
    - Changed file "gameManager" to "GameManager" to fix scripts referencing "GameManager"

--- Assets/Scripts/ReticleRaycast.cs
+++ Assets/Scripts/ReticleRaycast.cs
@@ -42,28 +42,28 @@ public class ReticleRaycast : MonoBehaviour {
 				// Debug.Log ("Spooky");
 			}
 
-			/* If the player clicks an object that can be picked up, add it to the inventory */
-			if (Input.GetMouseButtonDown (0) && GameManager.canClick == true && hit.collider.gameObject.tag == "inventoryItem") {
-				// GameManager.currentClick = hit.transform.gameObject;
-				InventoryManager.AddObject (hit.collider.gameObject);
-				changeSpeed(hit.collider.gameObject.GetComponent<ItemAttribute>().Weight);
-			}
-
-			/* If the player clicks the safe, check if the player has all 4 pieces of the combination */
-			if (Input.GetMouseButtonDown (0) && GameManager.canClick == true && hit.collider.gameObject.tag == "safe") {
-				if (GameManager.passcode == 4) {
-					Debug.Log ("Bingo");
-				} else {
-					StartCoroutine(UI_Manager.Message ("I don't have the entire combination..."));
+				/* If the player clicks an object that can be picked up, add it to the inventory */
+				if (Input.GetMouseButtonDown (0) && GameManager.canClick == true && hit.collider.gameObject.tag == "inventoryItem") {
+					// GameManager.currentClick = hit.transform.gameObject;
+					InventoryManager.AddObject (hit.collider.gameObject);
+					changeSpeed(hit.collider.gameObject.GetComponent<ItemAttribute>().Weight);
 				}
-			}
 
-			/* If the player clicks an object tagged "passcode", destroy that object,
-			and the player now has part of the passcode! */
-			if (Input.GetMouseButtonDown (0) && GameManager.canClick == true && hit.collider.gameObject.tag == "passcode") {
-				GameManager.passcode += 1;
-				Destroy(hit.collider.gameObject);
-			}
+				/* If the player clicks the safe, check if the player has all 4 pieces of the combination */
+				if (Input.GetMouseButtonDown (0) && GameManager.canClick == true && hit.collider.gameObject.tag == "safe") {
+					if (GameManager.passcode == 4) {
+						Debug.Log ("Bingo");
+					} else {
+						StartCoroutine(UI_Manager.Message ("I don't have the entire combination..."));
+					}
+				}
+
+				/* If the player clicks an object tagged "passcode", destroy that object,
+				and the player now has part of the passcode! */
+				if (Input.GetMouseButtonDown (0) && GameManager.canClick == true && hit.collider.gameObject.tag == "passcode") {
+					GameManager.passcode += 1;
+					Destroy(hit.collider.gameObject);
+				}
 		}
 		else {
 			AspectRatio.rectTransform.localScale = new Vector3(1,1.35f,1);
