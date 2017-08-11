7674ae6bc3d51bd21202684a66a5af616be2bdf5
Nstjacques <Nstjacques15bf@gmail.com>
2017-08-11 15:16:38 -0400

    AI Work
    -Starting work on AI stuff

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -1,6 +1,9 @@
 ﻿// Patrol.cs
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.AI;
 
 /* Hi Nick! This script is for creating a really basic patrolling AI using the [Unity NavMesh](https://docs.unity3d.com/Manual/nav-BuildingNavMesh.html)
@@ -10,8 +13,22 @@ you want to move on a patrol, and makes it find the next one! V simple, v useful
 public class Patrol : MonoBehaviour {
 
 	public Transform[] points;
-	private int destPoint = 0;
+	private int destPoint = 1;
 	private NavMeshAgent agent;
+	private bool playerInRange;
+	private Vector3 playerTransform;
+
+	public GameObject playerObject;
+
+	private Ray raycast;
+
+	private int lastPoint;
+
+	private bool isInside;
+
+	private Vector3 fromPosition;
+	private Vector3 toPosition;
+	private Vector3 direction;
 
 
 	void Start () {
@@ -37,13 +54,54 @@ public class Patrol : MonoBehaviour {
 		// Choose the next point in the array as the destination,
 		// cycling to the start if necessary.
 		destPoint = (destPoint + 1) % points.Length;
+
+		if (destPoint == 0) {
+			destPoint += 1;
+		}
 	}
 
 
 	void Update () {
 		// Choose the next destination point when the agent gets
 		// close to the current one.
-		if (agent.remainingDistance < 0.1f)
-			GotoNextPoint();
+		playerTransform = playerObject.transform.position;
+
+		if (agent.remainingDistance < 0.1f) {
+			GotoNextPoint ();
+		}
+
+		if (playerInRange) {
+			Vector3 fromPosition = this.transform.position;
+			Vector3 toPosition = playerObject.transform.position;
+			Vector3 direction = toPosition - fromPosition;
+
+			RaycastHit hit = new RaycastHit ();
+
+			if (Physics.Raycast (transform.position, direction, out hit)) {
+				print ("sendingGoodVibes");
+				if (hit.collider.gameObject.tag != "environment" && hit.collider.gameObject.tag == "player") {
+					print ("booming");
+
+					lastPoint = destPoint;
+
+					destPoint = 0;
+
+					agent.destination = playerTransform;
+
+					if (playerInRange = false) {
+						destPoint = lastPoint;
+						GotoNextPoint ();
+					}
+				}
+			}
+		}
+	}
+
+	private void OnTriggerEnter(Collider other){
+		playerInRange = (other.tag == "player");
+	}
+
+	private void OnTriggerExit(Collider other){
+		playerInRange = (other.tag == "player");
 	}
 }
